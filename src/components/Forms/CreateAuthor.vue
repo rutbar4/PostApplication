1cb469cd60c0374f377857c9ac991992c7a0569d
Rūta B <ruta.bartkute@teltonika.lt>
2023-11-27 09:42:05 +0200

    Resolve "Create author"

--- src/components/Forms/CreateAuthor.vue
+++ src/components/Forms/CreateAuthor.vue
@@ -1,5 +1,37 @@
 <template>
 	<div id="create-author">
-		<div>Create author</div>
+		<form>
+			<div
+				class="field"
+				style="padding-left: 20%; padding-right: 20%"
+			>
+				<div>Create author</div>
+				<input
+					class="input"
+					placeholder="Name"
+					type="text"
+					v-model="authorName"
+				/>
+				<div style="padding-top: 10px">
+					<button v-on:click="post_author(authorName)">Sumbit</button>
+				</div>
+			</div>
+		</form>
 	</div>
 </template>
+
+<script>
+	import { mapActions, mapMutations } from 'vuex';
+	export default {
+		name: 'CreateAuthor',
+		data() {
+			return {
+				authorName: '',
+			};
+		},
+		methods: {
+			...mapActions(['post_author']),
+			...mapMutations(['SET_NAME']),
+		},
+	};
+</script>
--- src/store/plugins/apiPlugin.js
+++ src/store/plugins/apiPlugin.js
@@ -11,6 +11,21 @@ const APIPlugin = (store) => {
 			throw new Error('There was a problem fetching posts from the server');
 		}
 	};
+
+	store.postData = async function (url, name) {
+		try {
+			console.log("postData");
+			const response = await this.http.post(`/${url}`, {
+				id: Math.random * 10,
+				name: name,
+				created_at: new Date().toLocaleDateString(),
+				updated_at: new Date().toLocaleDateString(),
+			});
+			return response.data;
+		} catch (error) {
+			throw new Error('There was a problem posting an author');
+		}
+	};
 };
 
 export default APIPlugin;
